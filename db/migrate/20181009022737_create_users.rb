9037d7e4a0a3282d752805dd76376dc2288d3983
SedielRuiz21@gmail.com <SedielRuiz21@gmail.com>
2018-10-08 22:00:26 -0500

    base line

---
+++ db/migrate/20181009022737_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :last_name
+      t.integer :phone
+      t.string :address
+      t.string :email
+      t.string :user_name
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
