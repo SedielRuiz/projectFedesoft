07c3a8dfaaebfb0e44d096b33f1610392fba5c4c
SedielRuiz <SedielRuiz21@gmail.com>
2018-10-27 13:22:58 -0500

    fix get token neighborhoods

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,5 +1,5 @@
 class NeighborhoodsController < ApplicationController
-  before_action :authenticate_user, except: [:create]
+  before_action :authenticate_user, except: [:create, :index]
   before_action :set_neighborhood, only: [:show, :update, :destroy]
 
   # GET /neighborhoods
