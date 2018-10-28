fc11ada393e55749aa3a2c13b14f589f6f9f4a7c
SedielRuiz <SedielRuiz21@gmail.com>
2018-10-28 11:14:48 -0500

    fix configuration mailer production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, except: [:create]
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:show, :update, :destroy, :signIn]
 
   # GET /users
   def index
