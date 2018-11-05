7227b3e23d4098ceb04da5d8abb797d674ddae91
SedielRuiz <SedielRuiz21@gmail.com>
2018-11-05 11:25:46 -0500

    fix serialize

--- app/serializers/poll_serializer.rb
+++ app/serializers/poll_serializer.rb
@@ -3,8 +3,8 @@ class PollSerializer < ActiveModel::Serializer
   has_many :questions
 
   class QuestionSerializer < ActiveModel::Serializer
-    has_many :options_question
     attributes :id, :concept_quiestion, :value_question
+    belongs_to :options_question
 
     class OptionsQuestionSerializer < ActiveModel::Serializer      
       attributes :id, :description_opction, :question_id
