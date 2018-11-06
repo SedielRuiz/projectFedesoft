6694ebd83e5f629327f75c7669a0db0dfe91ff5b
SedielRuiz <SedielRuiz21@gmail.com>
2018-11-05 20:25:03 -0500

    fix serializer comments_forum

--- app/serializers/comments_forum_serializer.rb
+++ app/serializers/comments_forum_serializer.rb
@@ -1,6 +1,7 @@
 class CommentsForumSerializer < ActiveModel::Serializer
   attributes :id, :comment_forum
-  belongs_to :comments_forums
+  has_many :user
+  
   class UserSerializer < ActiveModel::Serializer
     attributes :id, :user_name
   end
