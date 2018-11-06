2d8bea40a3a142cfff4ccd9431734c56c8bd6eae
SedielRuiz <SedielRuiz21@gmail.com>
2018-11-05 20:32:32 -0500

    delete serializer comments_forum

--- app/serializers/comments_forum_serializer.rb
+++
@@ -1,8 +0,0 @@
-class CommentsForumSerializer < ActiveModel::Serializer
-  attributes :id, :comment_forum
-  has_many :users
-  
-  class UserSerializer < ActiveModel::Serializer
-    attributes :id, :user_name
-  end
-end
