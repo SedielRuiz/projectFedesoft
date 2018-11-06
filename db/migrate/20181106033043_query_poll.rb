d8abbdd65eac5452916a073e281712bbf6ccf5d3
SedielRuiz <SedielRuiz21@gmail.com>
2018-11-05 22:40:56 -0500

    fix migraction query

--- db/migrate/20181106033043_query_poll.rb
+++ db/migrate/20181106033043_query_poll.rb
@@ -1,9 +1,9 @@
 class QueryPoll < ActiveRecord::Migration[5.2]
   def change
     remove_column :result_surveys :poll_id, :integer
-    t.references :apply_survey, index: true
-    add_foreign_key :result_surveys, :apply_surveys
+    add_foreign_key :apply_survey, :apply_surveys
 
     add_column :options_questions, :value_option, :integer
+
   end
 end
