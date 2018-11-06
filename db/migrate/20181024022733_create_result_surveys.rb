ea1528d7aee4c4a4b8ab2bd1e6eb42db6b52b5b5
SedielRuiz <SedielRuiz21@gmail.com>
2018-11-05 22:36:47 -0500

    fix migration with other migration

--- db/migrate/20181024022733_create_result_surveys.rb
+++ db/migrate/20181024022733_create_result_surveys.rb
@@ -4,15 +4,14 @@ class CreateResultSurveys < ActiveRecord::Migration[5.2]
       t.bit :answer
 
       #foranea encuestas
-      t.references :poll, index: true
+      t.integer :poll_id
+      t.references :poll_id, index: true
       #foranea usuario
       t.references :question, index: true
-      #foranea aplicación
-      t.references :apply_survey, index: true
+
       t.timestamps
     end
     add_foreign_key :result_surveys, :polls
     add_foreign_key :result_surveys, :questions
-    add_foreign_key :result_surveys, :apply_surveys
   end
 end
